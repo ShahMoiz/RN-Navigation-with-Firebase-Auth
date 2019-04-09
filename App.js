19e686ac07d3194c46095d332e17ba18b43f148a
shahMoiz <abdul_moiz10@hotmail.com>
2019-04-09 12:05:53 -0700

    All initial Libraries Install (Firebase + Navigation + Icons

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class App extends Component{
   render(){
     return(
       <View>
         <Text>App.js Works</Text>
+        <Icon name="home" sizee={30}/>
       </View>
     )
   }
--- android/app/src/main/java/com/rn_navigation_with_firebase_auth/MainApplication.java
+++ android/app/src/main/java/com/rn_navigation_with_firebase_auth/MainApplication.java
@@ -3,15 +3,18 @@ package com.rn_navigation_with_firebase_auth;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
+import com.oblador.vectoricons.VectorIconsPackage; // for Vector Icons
+
+import com.reactnativenavigation.NavigationApplication; // for Navigation
+import com.reactnativenavigation.react.NavigationReactNativeHost; // for Navigation
+import com.reactnativenavigation.react.ReactGateway; // for Navigation
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,6 +41,7 @@ public class MainApplication extends NavigationApplication {
                     // No need to add RnnPackage and MainReactPackage
                             return Arrays.<ReactPackage>asList(
                         // eg. new VectorIconsPackage()
+                                    new VectorIconsPackage()
                             );
         }
 
