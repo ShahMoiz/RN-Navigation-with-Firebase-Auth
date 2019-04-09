3b9150db18cc0f8772a2a33a618497c288f637cb
shahMoiz <abdul_moiz10@hotmail.com>
2019-04-09 11:53:47 -0700

    Navigation Add

--- android/app/src/main/java/com/rn_navigation_with_firebase_auth/MainActivity.java
+++ android/app/src/main/java/com/rn_navigation_with_firebase_auth/MainActivity.java
@@ -1,15 +1,8 @@
 package com.rn_navigation_with_firebase_auth;
 
 import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "RN_Navigation_With_Firebase_Auth";
-    }
 }
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
+//import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+//import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => App);
+
+
+import { Navigation } from "react-native-navigation";
+
+Navigation.registerComponent('Main.Component', () => App);
+
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            component: {
+                name: 'Main.Component'
+            }
+        }
+    })
+})
